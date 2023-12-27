fcb01ef403670f9fc07d3f435e668ef60f6821cc
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-12-27 20:28:02 +0530

    catalog path correction

--- catalogue.sh
+++ catalogue.sh
@@ -3,19 +3,21 @@ dnf module enable nodejs:18 -y
 
 dnf install nodejs -y
 useradd roboshop
-npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
+
+
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
+npm install
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
 cd roboshop-shell
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org-shell -y
 
 mongo --host mongodb-dev.nldevopsb01.online </app/schema/catalogue.js
